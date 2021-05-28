6c8fdb2a223da85a7bc011cd7d3687e6beb396da
technofoible <techno.foible@gmail.com>
2021-05-28 11:45:48 -0500

    Update readme. Add player class and get input from user.

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # mastermind
 
 
-Command line based game based off mastermind game.
+Command line based game loosely based off mastermind game. 
+Then I read the wikipedia page and thought "bulls and cows" wsa just a more fun name so I copied that instead.
 
 Made as part of The Odin Project curriculum.
--- mastermind.rb
+++ mastermind.rb
@@ -1,16 +1,58 @@
 class Mastermind
   def initialize
-    secret_code = 1234
+    @player1 = Player.new('Player 1')
+    @player2 = Player.new('Player 2')
+
+    @current_player = @player1
+
+    @current_secret = @current_player.make_secret
   end
 
-  def begin_game
+  def play
     puts "Starting new game."
-    puts "Make a guess?"
-    
+    @guess = @current_player.make_guess
+    check_game_over(@guess)
+
+
+  end
+
+  # check game over
+  def check_game_over(guess)
+    if guess == @current_secret
+      puts "You win!"
+    else
+      puts "Guess again."
+    end
+  end
+
+  # make_hint
+  def make_hint
+
+  end
+
+end
+
+class Player
+  # initialize
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  # make_secret
+  def make_secret
+    secret = 1234
+  end
+
+  # make_guess
+  def make_guess
+    puts "#{self.name} make a guess?" 
     guess = gets.chomp.to_i
     puts "You guessed #{guess}."
+    return guess
   end
 end
 
 new_game = Mastermind.new
-new_game.begin_game
+new_game.play
